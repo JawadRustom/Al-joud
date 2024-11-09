5f6de3794bf582a66b9ef56d22d059e04949ff3d
JawadRustom <jawad.ru1@gmail.com>
2024-11-09 04:02:07 +0300

    add sanctum auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,12 +11,13 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser, HasName
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasApiTokens,HasFactory, Notifiable, HasRoles;
 
 //    public function canAccessPanel(Panel $panel): bool
 //    {
